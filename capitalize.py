291132e19048b50c576ce856ee8918ad3053becc
Sandeep Pal <skumar8work@gmail.com>
2022-12-18 18:25:09 +0530

    Create capitalize.py HackerRank Solutions😊

---
+++ capitalize.py
@@ -0,0 +1,12 @@
+# Complete the solve function below.
+def solve(s):
+    for i in s.split():
+        s = s.replace(i,i.capitalize())
+    return s
+
+    if __name__ == '__main__':
+        fptr = open(os.environ['OUTPUT_PATH'], 'w')
+        s = input()
+        result = solve(s)
+        fptr.write(result + '\n')
+        fptr.close()
